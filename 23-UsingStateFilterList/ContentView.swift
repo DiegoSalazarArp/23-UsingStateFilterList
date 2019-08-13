52a7f52923b5899b0f2221aec942eca5d98949cd
Diego Salazar Arp <d.salazararp@gmail.com>
2019-08-13 17:54:09 -0400

    Initial Commit

---
+++ 23-UsingStateFilterList/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  23-UsingStateFilterList
+//
+//  Created by Diego Salazar Arp on 13-08-19.
+//  Copyright © 2019 Diego Salazar Arp. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
